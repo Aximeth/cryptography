2b64f5398c7ea3ac3fa9d309a2da2322fa8344dc
elcel <elcel@DESKTOP-V7I9EF4>
2022-06-13 22:03:18 +0300

    Add project files.

---
+++ cryptography/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace cryptography
+{
+    partial class cryptographyMachine
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.encryptTextBox = new System.Windows.Forms.TextBox();
+            this.encryptedTextBox = new System.Windows.Forms.TextBox();
+            this.decryptedTextBox = new System.Windows.Forms.TextBox();
+            this.decryptTextBox = new System.Windows.Forms.TextBox();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(671, 28);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(114, 35);
+            this.encryptButton.TabIndex = 0;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // encryptTextBox
+            // 
+            this.encryptTextBox.Location = new System.Drawing.Point(25, 28);
+            this.encryptTextBox.Name = "encryptTextBox";
+            this.encryptTextBox.Size = new System.Drawing.Size(640, 35);
+            this.encryptTextBox.TabIndex = 1;
+            // 
+            // encryptedTextBox
+            // 
+            this.encryptedTextBox.Location = new System.Drawing.Point(25, 78);
+            this.encryptedTextBox.Name = "encryptedTextBox";
+            this.encryptedTextBox.Size = new System.Drawing.Size(760, 35);
+            this.encryptedTextBox.TabIndex = 2;
+            // 
+            // decryptedTextBox
+            // 
+            this.decryptedTextBox.Location = new System.Drawing.Point(25, 276);
+            this.decryptedTextBox.Name = "decryptedTextBox";
+            this.decryptedTextBox.Size = new System.Drawing.Size(760, 35);
+            this.decryptedTextBox.TabIndex = 5;
+            // 
+            // decryptTextBox
+            // 
+            this.decryptTextBox.Location = new System.Drawing.Point(25, 226);
+            this.decryptTextBox.Name = "decryptTextBox";
+            this.decryptTextBox.Size = new System.Drawing.Size(640, 35);
+            this.decryptTextBox.TabIndex = 4;
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(671, 226);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(114, 35);
+            this.decryptButton.TabIndex = 3;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // versionLabel
+            // 
+            this.versionLabel.AutoSize = true;
+            this.versionLabel.ForeColor = System.Drawing.Color.Orange;
+            this.versionLabel.Location = new System.Drawing.Point(666, 314);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Size = new System.Drawing.Size(133, 30);
+            this.versionLabel.TabIndex = 6;
+            this.versionLabel.Text = "version: 1.0.0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(20, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 30);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "only string data acceptable !";
+            // 
+            // cryptographyMachine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(799, 351);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.versionLabel);
+            this.Controls.Add(this.decryptedTextBox);
+            this.Controls.Add(this.decryptTextBox);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.encryptedTextBox);
+            this.Controls.Add(this.encryptTextBox);
+            this.Controls.Add(this.encryptButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "cryptographyMachine";
+            this.Text = "Cryptography Machine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.TextBox encryptTextBox;
+        private System.Windows.Forms.TextBox encryptedTextBox;
+        private System.Windows.Forms.TextBox decryptedTextBox;
+        private System.Windows.Forms.TextBox decryptTextBox;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.Label versionLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ cryptography/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cryptography
+{
+    public partial class cryptographyMachine : Form
+    {
+        public cryptographyMachine()
+        {
+            InitializeComponent();
+        }
+        RsaEncryption rsa = new RsaEncryption();
+        string cypher = string.Empty;
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            var text = encryptTextBox.Text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                encryptedTextBox.AppendText(rsa.Encrypt(text));
+            }
+        }
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            var text = decryptTextBox.Text;
+            var plainText = rsa.Decrypt(text);
+            decryptedTextBox.AppendText(plainText);
+        }
+    }
+}
